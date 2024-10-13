cfef27576bf634214e421d424b4f07725f032153
akielaries <akiel@akiel.org>
2024-10-13 04:04:17 +0000

    updating comms interface

--- comms.h
+++ comms.h
@@ -15,7 +15,37 @@ typedef struct {
   /**
    * @brief initialize comms + type
    */
-  int (*init)(comm_context_t *ctx)
+  int (*init)(comm_context_t *ctx);
+  /** 
+   * @brief read one byte from the comms stream
+   *
+   * @return byte read, some meaningful errno on failure
+   */
+  int (*read_one)(comm_context_t *ctx, uint16_t timeout_ms);
+  /** 
+   * @brief read a buffer of bytes from the comms stream
+   *
+   * @return bytes read, < 0 on failure
+   */
+  int (*read)(comm_context_t *ctx, uint32_t *rx, uint16_t rx_sz, uint16_t timeout_ms);
+  /** 
+   * @brief read a buffer of random bytes from the stream
+   *
+   * @param[in] ctx       comm context pointer
+   * @param[in] data      single uint32 input
+   * @param[in] timout_ms timeout after given time
+   *
+   * @return bytes read, < 0 on failure
+   */
+  int (*write_one)(comm_context_t *ctx, uint32_t tx, uint16_t timeout_ms);
+  /**
+   * @brief write a buffer of bytes to the comms stream
+   *
+   *
+   * @return bytes read, < 0 on failure
+   */
+  int (*write)(comm_context_t *ctx, uint32_t *tx, uint16_t tx_sz, uint16_t timeout_ms);
+
 } comm_driver_t;
 
 #endif
