4288c8864467eb5babcb228d8977424a836bd435
akielaries <akiel@akiel.org>
2024-10-13 03:57:29 +0000

    avalanche noise generator RNG stubs

--- avalanche.c
+++ avalanche.c
@@ -1,6 +1,44 @@
+#include <stdint.h>
+
 #include "driver.h"
+#include "comms/comms.h"
 
-noise_driver_t avalanche_ops {
 
+rng_driver_t avalanche_ops {
+  .init = init,
+  .reset = reset,
+  .read_one = read_one,
+  .read = read,
+  .ioctl = ioctl,
 };
 
+
+static int init(rng_context_t *ctx) {
+  int ret = 0;
+
+  return ret;
+}
+
+static int reset(rng_context_t *ctx) {
+  int ret = 0;
+
+  return ret;
+}
+
+static int read_one(rng_context_t *ctx, uint16_t timeout_ms) {
+  int read_bytes = 0;
+
+  return read_bytes;
+}
+
+static int read(rng_context_t *ctx, uint32_t *buf, uint16_t buf_sz, uint16_t timeout_ms) {
+  int read_bytes = 0;
+
+  return read_bytes;
+}
+
+static int ioctl(rng_context_t *ctx, uint32_t opcode, void *data) {
+  int ret = 0;
+
+  return ret;
+}
