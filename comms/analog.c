15904d5ea763695ec527c33d5f1599f8d5e4fddd
akielaries <akiel@akiel.org>
2024-10-13 05:17:31 +0000

    updating comms stubs

--- comms/analog.c
+++ comms/analog.c
@@ -3,32 +3,40 @@
 
 #include "comms.h"
 
-static int init(comm_context_t *ctx);
-static int read_one(comm_context_t *ctx, uint16_t timeout_ms);
-static int read(comm_context_t *ctx, uint32_t *rx, uint16_t rx_sz, uint16_t timeout_ms);
+static int analog_init(comm_context_t *ctx);
+static int analog_read_one(comm_context_t *ctx, uint16_t timeout_ms);
+static int analog_read(comm_context_t *ctx, uint32_t *rx, uint16_t rx_sz, uint16_t timeout_ms);
+static int analog_ioctl(comm_context_t *ctx, uint32_t opcode, void *data);
 
 comm_driver_t analog_ops = {
-  .init = init,
-  .read_one = read_one,
-  .read = read,
-  .write_one = NULL,  /** @brief I only want to perform analog reads */
-  .write = NULL,      /** @brief I only want to perform analog reads */
+  .init       = analog_init,
+  .read_one   = analog_read_one,
+  .read       = analog_read,
+  .write_one  = NULL, /** @brief I only want to perform analog reads */
+  .write      = NULL, /** @brief I only want to perform analog reads */
+  .ioctl      = analog_ioctl,
 };
 
-static int init(comm_context_t *ctx) {
+static int analog_init(comm_context_t *ctx) {
   int ret = 0;
 
   return ret;
 }
 
-static int read_one(comm_context_t *ctx, uint16_t timeout_ms) {
+static int analog_read_one(comm_context_t *ctx, uint16_t timeout_ms) {
   int bytes_read = 0;
 
   return bytes_read;
 }
 
-static int read(comm_context_t *ctx, uint32_t *rx, uint16_t rx_sz, uint16_t timeout_ms) {
+static int analog_read(comm_context_t *ctx, uint32_t *rx, uint16_t rx_sz, uint16_t timeout_ms) {
   int bytes_read = 0;
 
   return bytes_read;
+}
+
+static int analog_ioctl(comm_context_t *ctx, uint32_t opcode, void *data) {
+  int ret = 0;
+
+  return ret;
 }
--- comms/uart.c
+++ comms/uart.c
@@ -1,3 +1,41 @@
 #include <stdint.h>
 #include "comms.h"
 
+
+static int uart_init(comm_context_t *ctx);
+static int uart_read_one(comm_context_t *ctx, uint16_t timeout_ms);
+static int uart_read(comm_context_t *ctx, uint32_t *rx, uint16_t rx_sz, uint16_t timeout_ms);
+static int uart_ioctl(comm_context_t *ctx, uint32_t opcode, void *data);
+
+comm_driver_t uart_ops = { 
+  .init       = uart_init,
+  .read_one   = uart_read_one,
+  .read       = uart_read,
+  .write_one  = uart_write_one,
+  .write      = uart_write,
+};
+
+static int uart_init(comm_context_t *ctx) {
+  int ret = 0;
+
+  return ret;
+}
+
+static int uart_read_one(comm_context_t *ctx, uint16_t timeout_ms) {
+  int bytes_read = 0;
+
+  return bytes_read;
+}
+
+static int uart_read(comm_context_t *ctx, uint32_t *rx, uint16_t rx_sz, uint16_t timeout_ms) {
+  int bytes_read = 0;
+
+  return bytes_read;
+}
+
+static int uart_ioctl(comm_context_t *ctx, uint32_t opcode, void *data) {
+  int ret = 0;
+
+  return ret;
+}
+
