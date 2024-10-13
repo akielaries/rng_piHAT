cb7a46590ced672cb9c55ed96dc7ce12be197bc6
akielaries <akiel@akiel.org>
2024-10-13 23:06:55 +0000

    cli

--- cli.c
+++ cli.c
@@ -1,24 +1,8 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    int fd = open("/dev/urandom", O_RDONLY);
-    if (fd == -1) {
-        perror("Failed to open /dev/urandom");
-        return 1;
-    }
+  printf("CLI util to toggle the MUX that switches between RNGs on /dev/serial0\n");
 
-    unsigned int random_value;
-    if (read(fd, &random_value, sizeof(random_value)) == -1) {
-        perror("Failed to read");
-        close(fd);
-        return 1;
-    }
-
-    printf("Random value: %u\n", random_value);
-    close(fd);
-    return 0;
+  return 0;
 }
 
