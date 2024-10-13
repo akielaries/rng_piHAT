427dfc9195b95f0ec87c99a3e2583be574de31de
akielaries <akiel@akiel.org>
2024-10-13 05:17:53 +0000

    updating rng circuit driver stubs

--- rng/avalanche.c
+++ rng/avalanche.c
@@ -1,43 +1,49 @@
 #include <stdint.h>
 
-#include "rng/circuit_driver.h"
-#include "comms/comms.h"
+#include "circuit_driver.h"
+#include "comms.h"
+
+static int avalanche_init(rng_context_t *ctx);
+static int avalanche_reset(rng_context_t *ctx);
+static int avalanche_read_one(rng_context_t *ctx, uint16_t timeout_ms);
+static int avalanche_read(rng_context_t *ctx, uint32_t *buf, uint16_t buf_sz, uint16_t timeout_ms);
+static int avalanche_ioctl(rng_context_t *ctx, uint32_t opcode, void *data);
 
 
-rng_driver_t avalanche_ops {
-  .init = init,
-  .reset = reset,
-  .read_one = read_one,
-  .read = read,
-  .ioctl = ioctl,
+rng_driver_t avalanche_ops = {
+  .init     = avalanche_init,
+  .reset    = avalanche_reset,
+  .read_one = avalanche_read_one,
+  .read     = avalanche_read,
+  .ioctl    = avalanche_ioctl,
 };
 
 
-static int init(rng_context_t *ctx) {
+static int avalanche_init(rng_context_t *ctx) {
   int ret = 0;
 
   return ret;
 }
 
-static int reset(rng_context_t *ctx) {
+static int avalanche_reset(rng_context_t *ctx) {
   int ret = 0;
 
   return ret;
 }
 
-static int read_one(rng_context_t *ctx, uint16_t timeout_ms) {
+static int avalanche_read_one(rng_context_t *ctx, uint16_t timeout_ms) {
   int read_bytes = 0;
 
   return read_bytes;
 }
 
-static int read(rng_context_t *ctx, uint32_t *buf, uint16_t buf_sz, uint16_t timeout_ms) {
+static int avalanche_read(rng_context_t *ctx, uint32_t *buf, uint16_t buf_sz, uint16_t timeout_ms) {
   int read_bytes = 0;
 
   return read_bytes;
 }
 
-static int ioctl(rng_context_t *ctx, uint32_t opcode, void *data) {
+static int avalanche_ioctl(rng_context_t *ctx, uint32_t opcode, void *data) {
   int ret = 0;
 
   return ret;
