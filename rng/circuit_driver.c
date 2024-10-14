3771910f7b4a61ce9c5efa196d4ff5923a6ff176
akielaries <akiel@akiel.org>
2024-10-14 00:58:03 +0000

    updates to rng and device reg

--- rng/circuit_driver.c
+++ rng/circuit_driver.c
@@ -1,7 +1,9 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <stddef.h>
 
 #include "circuit_driver.h"
+#include "comms.h"
 
 /** @brief forward declarations of available RNG drivers */
 extern rng_driver_t avalanche_ops;
@@ -25,6 +27,9 @@ static rng_context_t gl_rng_ctx;
 
 /** @brief device driver registry lookup */
 static rng_driver_t *find_rng_driver(rng_types_e type) {
+  //printf("finding RNG driver type {%d} from registry...\n", type);
+  LOG(LOG_LEVEL_INFO, "finding RNG driver type {%d} from registry...\n", type);
+
   for (int i = 0; rng_driver_registry[i].type != RNG_TYPES_END; ++i) {
     if (rng_driver_registry[i].type == type) {
       return rng_driver_registry[i].ops;
@@ -34,24 +39,22 @@ static rng_driver_t *find_rng_driver(rng_types_e type) {
 }
 
 /** brief top level initialize function */
-rng_context_t *init_rng(rng_types_e type) {
-    // Find the driver for the given RNG type
-    rng_driver_t *driver = find_rng_driver(type);
-    if (!driver || !driver->init) {
-        return NULL; // Driver not found or missing init function
-    }
+rng_context_t *rng_init(rng_types_e type) {
+  printf("initializing RNG driver with type %d...\n", type);
+  LOG(LOG_LEVEL_INFO, "intializing RNG driver type {%d}...\n", type);
 
-    // init the static context
-    gl_rng_ctx.type = type;
-    gl_rng_ctx.driver = driver;
+  // find the driver for the given RNG type
+  rng_driver_t *driver = find_rng_driver(type);
+  if (!driver || !driver->init) {
+    return NULL; // driver not found or missing init function
+  }
 
-    // ready to call the driver-specific init function
-    if (driver->init(&gl_rng_ctx) < 0) {
-        return NULL; // Initialization failed
-    }
-
-    return &gl_rng_ctx;
+  // init the static context
+  gl_rng_ctx.type = type;
+  gl_rng_ctx.driver = driver;
+  // ready to call the driver-specific init function
+  if (driver->init(&gl_rng_ctx) < 0) {
+    return NULL; // Initialization failed
+  }
+  return &gl_rng_ctx;
 }
-
-
-
