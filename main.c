ee0c5f3c77d2eac647f04eab94ff1ec033511490
akielaries <akiel@akiel.org>
2024-10-14 00:44:57 +0000

    updating main

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
+/**
+ *
+ * @description Driver entry point for interfacing with the various hardware
+ * random number generator circuits on the HAT. This file takes care of driver
+ * setup, initiating reading from analog pins mapped to an RNG, and streaming
+ * random data to one of the 3 serial transmission options. The firmware 
+ * running on the HAT accepts a mux to change the RNG -> serial output mapping
+ */
+
 #include <stdio.h>
 
-//#include "comms/comms.h"
 #include "rng/circuit_driver.h"
 
 
 int main (void) {
+  int ret = 0;
+
   printf("starting\n");
 
 
 
-  return 0;
+  return ret;
 }
